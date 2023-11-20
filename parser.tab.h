19eca0becfb351ab646e2197b5bbf7890b981d4f
pmahfuz <pedroom@al.insper.edu.br>
2023-11-20 16:12:52 -0300

    bison feito

--- parser.tab.h
+++ parser.tab.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.8.2.  */
+/* A Bison parser, made by GNU Bison 3.5.1.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
    Inc.
 
    This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -31,9 +31,8 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
-   especially those whose name start with YY_ or yy_.  They are
-   private implementation details that can be changed or removed.  */
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
 
 #ifndef YY_YY_PARSER_TAB_H_INCLUDED
 # define YY_YY_PARSER_TAB_H_INCLUDED
@@ -45,34 +44,29 @@
 extern int yydebug;
 #endif
 
-/* Token kinds.  */
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
   enum yytokentype
   {
-    YYEMPTY = -2,
-    YYEOF = 0,                     /* "end of file"  */
-    YYerror = 256,                 /* error  */
-    YYUNDEF = 257,                 /* "invalid token"  */
-    STRING = 258,                  /* STRING  */
-    WORD = 259,                    /* WORD  */
-    COMENTARIO = 260,              /* COMENTARIO  */
-    LEFT_PARENTHESIS = 261,        /* LEFT_PARENTHESIS  */
-    RIGHT_PARENTHESIS = 262,       /* RIGHT_PARENTHESIS  */
-    SAQUE = 263,                   /* SAQUE  */
-    REBATER = 264,                 /* REBATER  */
-    CORRER = 265,                  /* CORRER  */
-    MARCAR_PONTO = 266,            /* MARCAR_PONTO  */
-    FINALIZAR = 267,               /* FINALIZAR  */
-    TIPO = 268,                    /* TIPO  */
-    DIRECAO = 269,                 /* DIRECAO  */
-    BREAKLINE = 270,               /* BREAKLINE  */
-    SPACE = 271,                   /* SPACE  */
-    COMMA = 272,                   /* COMMA  */
-    ACAO = 273,                    /* ACAO  */
-    FORCA = 274                    /* FORCA  */
+    STRING = 258,
+    WORD = 259,
+    TITULO = 260,
+    LEFT_PARENTHESIS = 261,
+    RIGHT_PARENTHESIS = 262,
+    SAQUE = 263,
+    REBATER = 264,
+    CORRER = 265,
+    MARCAR_PONTO = 266,
+    FINALIZAR = 267,
+    TIPO = 268,
+    DIRECAO = 269,
+    BREAKLINE = 270,
+    SPACE = 271,
+    COMMA = 272,
+    ACAO = 273,
+    FORCA = 274
   };
-  typedef enum yytokentype yytoken_kind_t;
 #endif
 
 /* Value type.  */
@@ -85,8 +79,6 @@ typedef int YYSTYPE;
 
 extern YYSTYPE yylval;
 
-
 int yyparse (void);
-
 
 #endif /* !YY_YY_PARSER_TAB_H_INCLUDED  */
---
+++ parser.y
@@ -0,0 +1,53 @@
+%{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern int yylex();
+void yyerror(const char *s) { printf("ERROR: %s\n", s); }
+%}
+
+%token STRING WORD TITULO LEFT_PARENTHESIS RIGHT_PARENTHESIS SAQUE REBATER CORRER MARCAR_PONTO FINALIZAR TIPO DIRECAO BREAKLINE SPACE COMMA ACAO FORCA
+
+%start game
+
+%%
+game:
+    point BREAKLINE 
+    ;
+
+point:
+    header BREAKLINE dispute BREAKLINE point
+    | ACAO SPACE FINALIZAR
+    ;
+
+header:
+    TITULO SPACE title_line
+    ;
+
+title_line:
+    title
+    | title_line SPACE title
+    ;
+
+title:
+    WORD
+    ;
+
+dispute:
+    ACAO SPACE action  
+    ;
+
+action:
+    SAQUE LEFT_PARENTHESIS TIPO COMMA FORCA RIGHT_PARENTHESIS BREAKLINE dispute
+    | REBATER LEFT_PARENTHESIS DIRECAO COMMA FORCA RIGHT_PARENTHESIS BREAKLINE dispute
+    | CORRER LEFT_PARENTHESIS DIRECAO RIGHT_PARENTHESIS BREAKLINE dispute
+    | MARCAR_PONTO BREAKLINE
+    ;
+
+%%
+
+int main() {
+    yyparse();
+    return 0;
+}
